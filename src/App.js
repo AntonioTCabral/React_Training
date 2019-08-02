b74394b64183cbb0c2e270f6c680d33cf36b4dba
Antonio Cabral <antonio.tcabral@outlook.com>
2019-08-02 10:34:26 -0300

    Alteração de estilo inicial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   const generatePassword = () =>
   Math.random().toString().slice(3, 7);
@@ -9,13 +10,21 @@ function App() {
     const [password, setPassword] = useState(generatePassword());
 
     const handleClick = () => setPassword(generatePassword());
+    
   return (
-    <div className="App">
-      <h1>Triagem</h1>
-      <h2>Retire uma senha para ser atendido</h2>
-      <h3>Senha: {password}</h3>
-      <button onClick={() => handleClick()}>Gerar senha</button>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4 triagem">
+          <h1>Triagem</h1>
+          <h4>Retire uma senha para ser atendido</h4>
+          <div className="alert alert-success" role="alert">
+          <h3>Senha: {password}</h3>
+          </div>
+          <button className="btn btn-secondary" onClick={() => handleClick()}>Gerar senha</button>
+          </div>
+        </div>
+      </div>
+    
   );
 }
 
---
+++ src/include/popper.js
@@ -0,0 +1,3 @@
+import Popper from 'popper.js';
+
+Window.Popper = Popper;
