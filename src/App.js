be8f1897d3c2548e53bb5f690c9dcfd528a81c88
Antonio Cabral <antonio.tcabral@outlook.com>
2019-08-06 12:48:36 -0300

    Adição da logica de armazer as genhas que ja foram geradas

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
   const generatePassword = () =>
-  Math.random().toString().slice(3, 7);
+    Math.random().toString().slice(3, 7);
 
-    const [password, setPassword] = useState(generatePassword());
+  const [password, setPassword] = useState(0);
+  const [senhasGeradas,addSenhas] = useState([]);
 
-    const handleClick = () => setPassword(generatePassword());
+  
+  const gravaSenha = () => {
+    if (senhasGeradas.length >= 4){
+      senhasGeradas.shift();
+    }
+      addSenhas([
+      ...senhasGeradas,{
+        value : password
+      }
+    ])
+  
+  }
+
+  const handleClick = () => {
+    setPassword(generatePassword());
+    if (password > 0){
+    gravaSenha(password);
     
+    };
+   }
+
+
   return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-4 triagem">
+    <div className="container">
+      <div className="row container_principal ">
+        <div className="col-sm-4 triagem ">
           <h1>Triagem</h1>
           <h4>Retire uma senha para ser atendido</h4>
           <div className="alert alert-success" role="alert">
-          <h3>Senha: {password}</h3>
+            <h3>Senha: {password}</h3>
           </div>
-          <button className="btn btn-secondary" onClick={() => handleClick()}>Gerar senha</button>
+          <div className="btnGerador">
+            <button className="btn btn-secondary" onClick= {handleClick}>Gerar senha</button>
+          </div>
+        </div>
+        <div className="col-sm-4 senhasGeradas">
+          <h1>Senhas Geradas:</h1>
+          <div className="alert alert-success" role="alert">
+            <h3> {senhasGeradas.map(senhasGeradas => (
+              <h3>{senhasGeradas.value}</h3>))}
+            </h3>
           </div>
         </div>
       </div>
-    
+    </div>
+
   );
 }
 
